2ba153f40d0d5e9d22eb8ff36556e72d3032fd7d
dong959 <binz2028@gmail.com>
2024-01-03 15:32:25 +0700

    Build UI for registration and login pages

--- src/pages/Register/Register.tsx
+++ src/pages/Register/Register.tsx
@@ -1,3 +1,55 @@
+import { Link } from 'react-router-dom'
+
 export default function Register() {
-  return <div>Register</div>
+  return (
+    <div className='bg-orange'>
+      <div className='max-w-7xl mx-auto px-4'>
+        <div className='grid grid-cols-1 lg:grid-cols-5 py-12 lg:py-32 lg:pr-10'>
+          <div className='lg:col-span-2 lg:col-start-4'>
+            <form className='p-10 rounded bg-white shadow-sm'>
+              <div className='text-2xl'>Đăng ký</div>
+              <div className='mt-8'>
+                <input
+                  type='email'
+                  name='email'
+                  placeholder='Email'
+                  className='py-3 pl-3 w-full outline-none border border-gray-300 focus:border-gray-500 focus:shadow-color'
+                />
+                <div className='mt-1 text-red-600 text-sm .min-h-[1rem]'>Email không hợp lệ</div>
+              </div>
+              <div className='mt-3'>
+                <input
+                  type='password'
+                  name='password'
+                  placeholder='Password'
+                  className='py-3 pl-3 w-full outline-none border border-gray-300 focus:border-gray-500 focus:shadow-color'
+                />
+                <div className='mt-1 text-red-600 text-sm .min-h-[1rem]'>Mật khẩu không hợp lệ</div>
+              </div>
+              <div className='mt-3'>
+                <input
+                  type='password'
+                  name='confirm_password'
+                  placeholder='Confirm Password'
+                  className='py-3 pl-3 w-full outline-none border border-gray-300 focus:border-gray-500 focus:shadow-color'
+                />
+                <div className='mt-1 text-red-600 text-sm .min-h-[1rem]'>Mật khẩu không hợp lệ</div>
+              </div>
+              <div className='mt-3'>
+                <button className='w-full text-center py-4 px-2 uppercase bg-red-500 text-white text-sm hover:bg-red-600'>
+                  Đăng ký
+                </button>
+              </div>
+              <div className='flex items-center justify-center mt-8'>
+                <span className='text-slate-400 mr-1'>Bạn đã có tài khoản?</span>
+                <Link to='/login' className='text-red-400'>
+                  Đăng nhập
+                </Link>
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
 }
